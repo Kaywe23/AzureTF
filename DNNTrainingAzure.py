13ab83e15e2fc69348a1d0032ffa04a21c6eeb49
Widholm Kevin <Kevin.Widholm@novatec-gmbh.de>
2018-01-04 16:52:31 +0100

    first commit

--- DNNTrainingAzure.py
+++ DNNTrainingAzure.py
@@ -21,9 +21,7 @@ lemmatizer = WordNetLemmatizer()
 
 hidden_nodes_1 = 2638
 hidden_nodes_2 = int(hidden_nodes_1 * 0.5)
-hidden_nodes_3 = int(hidden_nodes_1 * np.power(0.5, 2))
-hidden_nodes_4 = int(hidden_nodes_1 * np.power(0.5, 3))
-hidden_nodes_5 = int(hidden_nodes_1 * np.power(0.5, 4))
+
 beta=0.001
 n_classes = 2
 batch_size = 100
@@ -35,107 +33,73 @@ display_step = 1
 graph = tf.Graph()
 with graph.as_default():
 
-    tf_train_dataset = tf.placeholder('float')
-    tf_train_labels = tf.placeholder('float')
+    with tf.name_scope('input'):
+        tf_train_dataset = tf.placeholder('float')
+        tf_train_labels = tf.placeholder('float')
 
-    # Hidden RELU layer 1
-    weights_1 = tf.Variable(tf.truncated_normal([2638, hidden_nodes_1], stddev=math.sqrt(2.0 / (2638))))
-    biases_1 = tf.Variable(tf.random_normal([hidden_nodes_1]))
+    with tf.name_scope('weights'):
+        weights_1 = tf.Variable(tf.truncated_normal([2638, hidden_nodes_1], stddev=math.sqrt(2.0 / (2638))))
+        weights_2 = tf.Variable(tf.truncated_normal([hidden_nodes_1, hidden_nodes_2], stddev=math.sqrt(2.0 / hidden_nodes_1)))
+        weights_3 = tf.Variable(tf.truncated_normal([hidden_nodes_2, n_classes], stddev=math.sqrt(2.0 / hidden_nodes_2)))
 
-    # Hidden RELU layer 2
-    weights_2 = tf.Variable(tf.truncated_normal([hidden_nodes_1, hidden_nodes_2], stddev=math.sqrt(2.0 / hidden_nodes_1)))
-    biases_2 = tf.Variable(tf.random_normal([hidden_nodes_2]))
+    with tf.name_scope('biases'):
+        biases_1 = tf.Variable(tf.random_normal([hidden_nodes_1]))
+        biases_2 = tf.Variable(tf.random_normal([hidden_nodes_2]))
+        biases_3 = tf.Variable(tf.random_normal([n_classes]))
 
-    # Hidden RELU layer 3
-    weights_3 = tf.Variable(tf.truncated_normal([hidden_nodes_2, hidden_nodes_3], stddev=math.sqrt(2.0 / hidden_nodes_2)))
-    biases_3 = tf.Variable(tf.random_normal([hidden_nodes_3]))
+    with tf.name_scope('deep neural net'):
 
-    # Hidden RELU layer 4
-    weights_4 = tf.Variable(tf.truncated_normal([hidden_nodes_3, hidden_nodes_4], stddev=math.sqrt(2.0 / hidden_nodes_3)))
-    biases_4 = tf.Variable(tf.random_normal([hidden_nodes_4]))
+        # Hidden RELU layer 1
+        logits_1 = tf.matmul(tf_train_dataset, weights_1) + biases_1
+        hidden_layer_1 = tf.nn.relu(logits_1)
 
-    # Hidden RELU layer 5
-    weights_5 = tf.Variable(tf.truncated_normal([hidden_nodes_4, hidden_nodes_5], stddev=math.sqrt(2.0 / hidden_nodes_4)))
-    biases_5 = tf.Variable(tf.random_normal([hidden_nodes_5]))
+        # Dropout on hidden layer: RELU layer
+        keep_prob = tf.placeholder("float")
+        hidden_layer_1_dropout = tf.nn.dropout(hidden_layer_1, keep_prob)
 
-    # Output layer
-    weights_6 = tf.Variable(tf.truncated_normal([hidden_nodes_5, n_classes], stddev=math.sqrt(2.0 / hidden_nodes_5)))
-    biases_6 = tf.Variable(tf.random_normal([n_classes]))
+        # Hidden RELU layer 2
+        logits_2 = tf.matmul(hidden_layer_1_dropout, weights_2) + biases_2
+        hidden_layer_2 = tf.nn.relu(logits_2)
 
+        # Dropout on hidden layer: RELU layer
+        hidden_layer_2_dropout = tf.nn.dropout(hidden_layer_2, keep_prob)
 
-    # Hidden RELU layer 1
-    logits_1 = tf.matmul(tf_train_dataset, weights_1) + biases_1
-    hidden_layer_1 = tf.nn.relu(logits_1)
+        # Output layer
+        logits_3 = tf.matmul(hidden_layer_2_dropout, weights_3) + biases_3
 
-    # Dropout on hidden layer: RELU layer
-    keep_prob = tf.placeholder("float")
-    hidden_layer_1_dropout = tf.nn.dropout(hidden_layer_1, keep_prob)
+    with tf.name_scope('cross_entropy'):
+        # Minimize error using cross entropy
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits_3, labels=tf_train_labels))
 
-    # Hidden RELU layer 2
-    logits_2 = tf.matmul(hidden_layer_1_dropout, weights_2) + biases_2
-    hidden_layer_2 = tf.nn.relu(logits_2)
+        # Loss function with L2 Regularization with decaying learning rate beta
+        regularizers = tf.nn.l2_loss(weights_1) + tf.nn.l2_loss(weights_2) + \
+                       tf.nn.l2_loss(weights_3)
+        cost = tf.reduce_mean(cost + beta * regularizers)
 
-    # Dropout on hidden layer: RELU layer
-    hidden_layer_2_dropout = tf.nn.dropout(hidden_layer_2, keep_prob)
+    with tf.name_scope('train'):
+        # Gradient Descent
+        #optimizer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
+        # Decaying learning rate
+        global_step = tf.Variable(0)  # count the number of steps taken.
+        start_learning_rate = 0.001
+        learning_rate = tf.train.exponential_decay(start_learning_rate, global_step, 100000, 0.96, staircase=True)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost, global_step=global_step)
 
-    # Hidden RELU layer 3
-    logits_3 = tf.matmul(hidden_layer_2_dropout, weights_3) + biases_3
-    hidden_layer_3 = tf.nn.relu(logits_3)
+    with tf.name_scope('Softmax'):
+        # Predictions for the training
+        train_prediction = tf.nn.softmax(logits_3)
 
-    # Dropout on hidden layer: RELU layer
-    hidden_layer_3_dropout = tf.nn.dropout(hidden_layer_3, keep_prob)
+    with tf.name_scope('Accuracy'):
+        correct = tf.equal(tf.argmax(train_prediction, 1), tf.argmax(tf_train_labels, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 
-    # Hidden RELU layer 4
-    logits_4 = tf.matmul(hidden_layer_3_dropout, weights_4) + biases_4
-    hidden_layer_4 = tf.nn.relu(logits_4)
-
-    # Dropout on hidden layer: RELU layer
-    hidden_layer_4_dropout = tf.nn.dropout(hidden_layer_4, keep_prob)
-
-    # Hidden RELU layer 5
-    logits_5 = tf.matmul(hidden_layer_4_dropout, weights_5) + biases_5
-    hidden_layer_5 = tf.nn.relu(logits_5)
-
-    # Dropout on hidden layer: RELU layer
-    hidden_layer_5_dropout = tf.nn.dropout(hidden_layer_5, keep_prob)
-
-    # Output layer
-    logits_6 = tf.matmul(hidden_layer_5_dropout, weights_6) + biases_6
-
-
-    # Minimize error using cross entropy
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits_6, labels=tf_train_labels))
-
-    # Loss function with L2 Regularization with decaying learning rate beta=0.5
-    regularizers = tf.nn.l2_loss(weights_1) + tf.nn.l2_loss(weights_2) + \
-                   tf.nn.l2_loss(weights_3) + tf.nn.l2_loss(weights_4) + \
-                   tf.nn.l2_loss(weights_5) + tf.nn.l2_loss(weights_6)
-    cost = tf.reduce_mean(cost + beta * regularizers)
-
-    # Gradient Descent
-    # optimizer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
-    # Decaying learning rate
-    global_step = tf.Variable(0)  # count the number of steps taken.
-    start_learning_rate = 0.001
-    learning_rate = tf.train.exponential_decay(start_learning_rate, global_step, 100000, 0.96, staircase=True)
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost, global_step=global_step)
-
-    # Predictions for the training
-    train_prediction = tf.nn.softmax(logits_6)
+    # summary
+    cost_summary = tf.summary.scalar("cost", cost)
+    acc_summary = tf.summary.scalar("accuracy", accuracy)
+    summary_op = tf.summary.merge_all()
 
     # define Checkpoint
     saver = tf.train.Saver()
-
-
-    # summary
-    correct = tf.equal(tf.argmax(logits_6, 1), tf.argmax(tf_train_labels, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
-    cost_summary = tf.summary.scalar("cost", cost)
-    acc_summary = tf.summary.scalar("accuracy", accuracy)
-
-    summary_op = tf.summary.merge_all()
-
-
 
 
 def trainDNN(train_file='lexikon2.pickle', csv_file='train_converted_vermischt.csv',
@@ -145,7 +109,7 @@ def trainDNN(train_file='lexikon2.pickle', csv_file='train_converted_vermischt.c
     lexikon = pickle.load(file_stream)
 
     # valid dataset
-    '''feature_sets = []
+    feature_sets = []
     labels = []
     with tf.gfile.Open(csv_file2, 'rb') as gc_file:
         lines2 = gc_file.readlines()
@@ -159,28 +123,30 @@ def trainDNN(train_file='lexikon2.pickle', csv_file='train_converted_vermischt.c
             except:
                 pass
 
-        test_1 = np.array(feature_sets)
-        valid_dataset = test_1
-
-        test_2 = np.array(labels)
-        valid_labels = test_2'''
-
+        valid_dataset = np.array(feature_sets)
+        valid_labels = np.array(labels)
 
 
     with tf.Session(graph=graph) as sess:
+
         tf.global_variables_initializer().run()
-        writer = tf.summary.FileWriter(job_dir, graph=graph)
+        print("Initialized")
+
+        writer = tf.summary.FileWriter(logs_path, graph=graph)
         print('Start Training')
-        try:
-            epoch = int(tf.gfile.Open(logs, 'r').read().split('\n')[-2]) + 1
-            print('START:', epoch)
-        except:
-            epoch = 1
+
+        #try:
+            #epoch = int(tf.gfile.Open(tf_log,'r').read().split('\n')[-2])+1
+            #print('START:',epoch)
+        #except:
+        epoch=1
 
         for epoch in range(hm_epochs):
+            t1=clock()
             #if epoch != 1:
                 #saver.restore(sess, checkpoint)
-            avg_cost = 0.
+            avg_cost=0.
+            avg_trainacc=0.
 
             with tf.gfile.Open(csv_file, 'rb') as gcs_file:
                 lines = gcs_file.readlines()
@@ -205,24 +171,27 @@ def trainDNN(train_file='lexikon2.pickle', csv_file='train_converted_vermischt.c
                                  keep_prob: 0.5}
                     _, c, predictions, summary = sess.run([optimizer, cost, train_prediction, summary_op],
                                                           feed_dict=feed_dict)
-                    writer.add_summary(summary, epoch * batch_count + zaehler)
+
+                    if zaehler % 10:
+                        writer.add_summary(summary, epoch * batch_count + zaehler)
 
                     avg_cost += c / batch_count
+                    avg_trainacc += (accuracy.eval(
+                        {tf_train_dataset: train_dataset, tf_train_labels: train_labels, keep_prob: 1.0})) / batch_count
 
                     if zaehler >= batch_count:
                         break
 
-                    if (zaehler % 100 == 0):
+                    if (zaehler % 1000 == 0):
                         print("Minibatch loss at step {}: {}".format(zaehler, c))
-                        #print("Minibatch accuracy: {:.9f}".format(accuracy.eval(
-                            #{tf_train_dataset: train_dataset, tf_train_labels: train_labels, keep_prob: 0.5})))
-                        #print("Validation accuracy: {:.9f}".format(accuracy.eval(
-                            #{tf_train_dataset: valid_dataset, tf_train_labels: valid_labels, keep_prob: 1.0})))
-
-
+                        print("Validation accuracy: {:.9f}".format(accuracy.eval(
+                            {tf_train_dataset: valid_dataset, tf_train_labels: valid_labels, keep_prob: 1.0})))
+                    t2 = clock()
             saver.save(sess, checkpoint)
+            t = t2 - t1
             if epoch % display_step == 0:
-                print "Epoche:", '%04d' % (epoch + 1), "of", '%04d' % (hm_epochs), "average cost=", "{:.9f}".format(avg_cost)
+                print "Epoche:", '%04d' % (epoch + 1), "of", '%04d' % (hm_epochs), "average cost=", "{:.9f}".format(avg_cost), \
+                        "Time=", "{:.9f}".format(t), "s,", "Epoch Trainaccuracy: {}".format(avg_trainacc)
 
             with tf.gfile.Open(logs, 'a') as f:
                 f.write(str(epoch) + '\n')
@@ -231,8 +200,6 @@ def trainDNN(train_file='lexikon2.pickle', csv_file='train_converted_vermischt.c
 
         feature_sets = []
         labels = []
-        zaehler = 0
-
         with tf.gfile.Open(csv_file2, 'rb') as gc_file:
             lines2 = gc_file.readlines()
 
@@ -242,22 +209,22 @@ def trainDNN(train_file='lexikon2.pickle', csv_file='train_converted_vermischt.c
                     label = list(eval(zeile.split('::')[1]))
                     feature_sets.append(features)
                     labels.append(label)
-                    zaehler += 1
                 except:
                     pass
 
-        test_x = np.array(feature_sets)
-        test_datasets = test_x
+            test_x = np.array(feature_sets)
+            test_datasets = test_x
 
-        test_y = np.array(labels)
-        test_labels = test_y
-        print('Test Set', test_datasets.shape, test_labels.shape)
+            test_y = np.array(labels)
+            test_labels = test_y
+            print('Test Set', test_datasets.shape, test_labels.shape)
 
-        writer.flush()
 
-        print 'Test Accuracy:', accuracy.eval(
-            {tf_train_dataset: test_datasets, tf_train_labels: test_labels, keep_prob: 1.0})
 
+            writer.flush()
+
+
+            print 'Test Accuracy:',accuracy.eval({tf_train_dataset: test_datasets, tf_train_labels: test_labels, keep_prob:1.0})
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
